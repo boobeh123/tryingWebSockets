ff97d6d78ba635c26cef990451c0a6c10c33deeb
Bobby <boobeh321@gmail.com>
2026-02-08 12:41:50 -1000

    Add notes

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // alert('hello world')
+
+/*************************************************************************
+* Notes
+*************************************************************************/
+/* 
+I forgot a lot about the process of creating a project from scratch, 
+as I started it the process, it starting coming back. 
+These notes may help on initializing a project, but is not a how-to. 
+It won't hold your hand, but provides (some) guidance
+
+- Create git repo & Initial commit
+- Create a package.json (npm init)
+- Install dependencies (express)
+- Create server.js
+- Run server with command: 'node server.js'
+- I forgot how to serve public files (css/js), so styling will be inline.
+    - I did spend time to attempt it. It was taking time away from webSockets
+- install more dependencies (socket.io)
+*/
