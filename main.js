50c2df99bfe3812c355074043a7d982e900fd4a3
Bobby <boobeh321@gmail.com>
2026-02-08 14:04:59 -1000

    Notes

--- main.js
+++ main.js
@@ -44,10 +44,40 @@ This snippet above is all it takes to load the socket.io-client
 
 There's no specific URL when I call `io()`, because it defaults to trying to connect to the host that serves the page
 
-Hosting the app that's not the root of the website (e.g., https://astroauction.com/chatapp) then we need to specify the path in both the server & the client
+Hosting the app that's not the root of the website (e.g., https://astroauction.com/announcements) then we need to specify the path in both the server & the client
 
 If you've reached this point, start the server & open several tabs to this app (localhost:3000)
 The terminal should display console logs (A User connected) each time a new client connects (the opening of several tabs to the URL localhost:3000)
 
+/*************************************************************************
+* Emitting events
+*************************************************************************/
+/*
+Socket.io can send & receive any event we want, with any data we want
 
+I plan to use this tool to create an "announcement banner" in an existing project
+
+In our index.html, we are using the <script> tag as our main.js / client-side javascript file, due to not serving my public dir to the server
+- Within this script tag, are adding an eventlistener to our form element.
+- When the form is submitted, 
+    - (we prevent the page from refreshing) 
+    - We determine if the input element contains a value (string/integers)
+        - If it the condition evaluates as true:
+            - We use Socket's built-in emit() method to send our input's value to the server as a chat message
+
+In short: When the user types in a message, the server gets it as a chat message event.
+Keep in mind: I have not used websockets before, so if something doesn't sound right, let me know
+
+In our server.js:
+- We are using Socket's built-in on() method and passing in two arguments, an eventName and a callback.
+    - eventName is a string and it represents the name of the event that we want to listen for.
+        - There are common built-in events such as: 'connect', 'connection', 'disconnect'.
+        - We may also define custom event names like: 'chat message'
+    - callback is a function that will be executed when the specified event is emitted
+        - When the callback is invoked, it receives arguments based on what was passed from the other side using `socket.emit()`
+            - Any additional arguments passed to `socket.emit()` are passed directly to `socket.on()`
+                - (e.g., input.value from our client-side js)
+
+* Note that socket.emit() was used in our client-side js & socket.on() was used in our server-side js
+    - Emit() is our sender & on() is our receiver
 */
